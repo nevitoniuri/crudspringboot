68cfeb97d0e9499009f087713501eaa51a425685
Iuri Neviton <iurineviton@gmail.com>
2021-09-24 11:04:11 -0300

    crud finished

--- src/main/java/br/com/nevitoniuri/crudspringboot/controller/GreetingsController.java
+++ src/main/java/br/com/nevitoniuri/crudspringboot/controller/GreetingsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -74,6 +75,39 @@ public class GreetingsController {
 		usuarioRepository.deleteById(idUsuario);
 		
 		return new ResponseEntity<String>("Usuario com ID " + idUsuario + " deletado com sucesso.", HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "buscaruserid") /*mapeia a url*/
+	@ResponseBody /* descricao da resposta */
+	public ResponseEntity<Usuario> buscaruserid (@RequestParam(name = "idUsuario") Long idUsuario){
+		
+		Usuario usuarioBuscado = usuarioRepository.findById(idUsuario).get();
+		
+		return new ResponseEntity<Usuario>(usuarioBuscado, HttpStatus.OK);
+	}
+	
+	
+	@PutMapping(value = "atualizar") /*mapeia a url*/
+	@ResponseBody /* descricao da resposta */
+	public ResponseEntity<?> atualizar (@RequestBody Usuario usuario){
+		
+		if(usuario.getId() == null) {
+			return new ResponseEntity<String> ("Id não foi informado ou não encontrado no Banco de Dados.", HttpStatus.OK);
+		}
+		
+		Usuario usuarioAtualizado = usuarioRepository.saveAndFlush(usuario);
+		
+		return new ResponseEntity<Usuario>(usuarioAtualizado, HttpStatus.CREATED);
+	}
+	
+	
+	@GetMapping(value = "buscarpornome") /*mapeia a url*/
+	@ResponseBody /* descricao da resposta */
+	public ResponseEntity<List<Usuario>> buscarPorNome (@RequestParam(name = "name") String name){
+		
+		List<Usuario> usuarioBuscado = usuarioRepository.buscarPorNome(name.trim().toUpperCase());
+		
+		return new ResponseEntity<List<Usuario>> (usuarioBuscado, HttpStatus.OK);
 	}
 	
 }
--- src/main/java/br/com/nevitoniuri/crudspringboot/repository/UsuarioRepository.java
+++ src/main/java/br/com/nevitoniuri/crudspringboot/repository/UsuarioRepository.java
@@ -1,6 +1,9 @@
 package br.com.nevitoniuri.crudspringboot.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import br.com.nevitoniuri.crudspringboot.model.Usuario;
@@ -8,4 +11,6 @@ import br.com.nevitoniuri.crudspringboot.model.Usuario;
 @Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long>{
 
+	@Query(value = "select u from Usuario u where upper(trim(u.nome)) like %?1%")
+	List<Usuario> buscarPorNome(String name);
 }
